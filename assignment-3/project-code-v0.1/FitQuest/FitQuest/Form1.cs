aff9f23504fe26b82d2b522e166b6c757e8a84f5
VasileiosBardakis <up1088098@ac.upatras.gr>
2024-05-10 19:31:10 +0300

    started reference

---
+++ assignment-3/project-code-v0.1/FitQuest/FitQuest/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitQuest
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            Profile profile = new Profile("John Doe", 25, 5);
+
+        }
+    }
+
+    public class Profile
+    {
+        // Variables for character profile
+        private string name;
+        private int age;
+        private int level;
+        private bool hasProgram;
+
+        // Constructor to initialize variables
+        public Profile(string name, int age, int level)
+        {
+            this.name = name;
+            this.age = age;
+            this.level = level;
+        }
+
+        // Method to display character profile
+        public bool getProfileHasProgram()
+        {
+            return hasProgram;
+        }
+    }
+}
