d626972bb0e3cc105f9f5f7d3810787117f519ac
VasileiosBardakis <up1088098@ac.upatras.gr>
2024-05-30 12:17:24 +0300

    Revert "Delete assignment-3 directory"
    This reverts commit 484604c6eba1d321d85dea19d69330143daf7519.

---
+++ assignment-3/project-code-v0.1/FitQuest/FitQuest/Item.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitQuest
+{
+    public class Item
+    {
+        private Hashtable stats = new Hashtable();
+        private string name;
+        private int value, quantity;
+        public enum categories
+        {
+            Weapon = 0,
+            Armour = 1,
+            Accessory = 2,
+            Consumable = 3
+        };
+        private categories category;
+
+        public Item(string name, int value, int quantity, categories category)
+        {
+            this.name = name;
+            this.value = value;
+            this.quantity = quantity;
+            this.category = category;
+            stats.Add("Damage", 5); // Example stat, can be changed
+        }
+
+        public categories getCategory()
+        {
+            return this.category;
+        }
+
+        public Hashtable GetStats()
+        {
+            return this.stats;
+        }
+    }
+}
